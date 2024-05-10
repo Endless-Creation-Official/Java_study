94901fb04b51edb9784322ad5cfa1481b051daf7
KT <kimgt2828@gmail.com>
2024-05-10 16:15:20 +0900

    기능들 모듈화하기

--- 김경태/Java_basic/src/oop1/MusicPlaterMain2.java
+++ 김경태/Java_basic/src/oop1/MusicPlaterMain2.java
@@ -7,31 +7,46 @@ public class MusicPlaterMain2 {
 
 
         //음악 플레이어 켜기
-        data.isOn = true;
-        System.out.println("음악 플레이어를 시작합니다");
+        on(data);
 
         // 볼륨 증가
-        data.volume++;
+        volumeUp(data);
         System.out.println("음악 플레이어 볼륨: " + data.volume);
 
         // 볼륨 증가
-        data.volume++;
+        volumeUp(data);
         System.out.println("음악 플레이어 볼륨: " + data.volume);
 
         // 볼륨 감소
-        data.volume--;
+        volumeDown(data);
         System.out.println("음악 플레이어 볼륨: " + data.volume);
 
         //음악 플레이어 상태
+        showStatus(data);
+        //음악 플레이어 끄기
+         off(data);
+    }
+    static void on(MusicPlayerData data) {
+        data.isOn = true;
+        System.out.println("음악 플레이어를 시작합니다.");
+    }
+    static void off(MusicPlayerData data) {
+        data.isOn = false;
+        System.out.println("음악 플레이어를 종료합니다.");
+    }
+    static void volumeUp(MusicPlayerData data){
+        data.volume++;
+    }
+    static void volumeDown(MusicPlayerData data) {
+        data.volume--;
+    }
+    static void showStatus(MusicPlayerData data) {
         System.out.println("음악 플레이어 상태 확인");
         if(data.isOn) {
             System.out.println("음악 플레이어 ON, 볼륨: " + data.volume);
         }else {
             System.out.println("음악 플레이어 OFF");
         }
-        //음악 플레이어 끄기
-         data.isOn = false;
-        System.out.println("음악 플레이어를 종료합니다.");
 
     }
 }
