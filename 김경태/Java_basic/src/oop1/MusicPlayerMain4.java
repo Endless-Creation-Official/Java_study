ae21eebbbe8907c3bdc72216364657c2c536daa5
KT <kimgt2828@gmail.com>
2024-05-10 23:34:13 +0900

    OOP1 연습 문제 1

---
+++ 김경태/Java_basic/src/oop1/MusicPlayerMain4.java
@@ -0,0 +1,18 @@
+package oop1;
+
+public class MusicPlayerMain4 {
+    public static void main(String[] args) {
+        MusicPlayer player = new MusicPlayer();
+        //음악 플레이어 켜기
+        player.on();
+        //볼륨 증가
+        player.volumeUp();
+        player.volumeUp();
+        //볼륨 감소
+        player.volumeDown();
+        //음악 플레이어 상태
+        player.showStatus();
+        //음악 플레이어 끄기
+        player.off();
+    }
+}
