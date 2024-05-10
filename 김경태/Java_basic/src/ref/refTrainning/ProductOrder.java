7ab4dfb922c82c7cfe723827299268b35901f20b
KT <kimgt2828@gmail.com>
2024-05-10 12:32:22 +0900

    참조형 연습문제 코드

---
+++ 김경태/Java_basic/src/ref/refTrainning/ProductOrder.java
@@ -0,0 +1,7 @@
+package ref.refTrainning;
+
+public class ProductOrder {
+    String productName;
+    int price;
+    int quantity;
+}
---
+++ 김경태/Java_basic/src/ref/refTrainning/ProductOrderMain2.java
@@ -0,0 +1,52 @@
+package ref.refTrainning;
+
+import java.util.Scanner;
+
+public class ProductOrderMain2 {
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("입력할 주문의 개수를 입력하세요: ");
+        int n = scanner.nextInt();
+        scanner.nextLine();
+
+        ProductOrder[] orders = new ProductOrder[n];
+
+        for(int i=0; i<n; i++) {
+            System.out.println(i+1 + "번째 주문 정보를 입력하세요.");
+            System.out.print("상품명: ");
+            String name = scanner.nextLine();
+            System.out.print("가격: ");
+            int price = scanner.nextInt();
+            System.out.print("수량: ");
+            int quantity = scanner.nextInt();
+            scanner.nextLine(); //입력 버퍼 비우기
+            orders[i] = createOrder(name, price, quantity);
+        }
+
+        printOrders(orders);
+        System.out.println("총 결제 금액: " + getTotalAmount(orders));
+
+
+    }
+
+    static ProductOrder createOrder(String productName, int price, int quantity){
+        ProductOrder order = new ProductOrder();
+        order.productName = productName;
+        order.price = price;
+        order.quantity = quantity;
+        return order;
+    }
+
+    static int getTotalAmount(ProductOrder orders[]) {
+        int amount = 0;
+        for(ProductOrder order: orders) amount += order.price * order.quantity;
+        return amount;
+    }
+    static  void printOrders(ProductOrder orders[]) {
+        for(ProductOrder order : orders) {
+            System.out.println("상품명: " + order.productName + ", 가격: " + order.price + ", 수량: " + order.quantity);
+        }
+    }
+}
