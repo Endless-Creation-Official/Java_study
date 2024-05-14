cebc6dc8a690afd4c69014c236951349dcd4dd27
KT <kimgt2828@gmail.com>
2024-05-14 11:10:00 +0900

    패키지 이름이 같을 경우 import 방법

---
+++ 김경태/Java_basic/src/pack/PackageMain3.java
@@ -0,0 +1,12 @@
+package pack;
+
+import pack.a.User;
+
+public class PackageMain3 {
+    public static void main(String[] args) {
+    User userA = new User();
+    pack.b.User userB = new pack.b.User();
+
+    }
+
+}
