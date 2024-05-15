b1538146b486887ae9518c3b96cbfceb65781408
KT <kimgt2828@gmail.com>
2024-05-15 12:09:56 +0900

    d

--- 김경태/Java_basic/src/access/trainning/MaxCounter.java
+++ 김경태/Java_basic/src/access/trainning/MaxCounter.java
@@ -1,2 +1,25 @@
-package access.trainning;public class MaxCounter {
+package access.trainning;
+
+public class MaxCounter {
+    private int count;
+    private int max;
+
+    public MaxCounter(int max) {
+        this.max = max;
+    }
+
+    public void increment() {
+        if (isOver(count)) {
+            System.out.println("최대 값입니다.");
+        }else {
+            count++;
+        }
+    }
+
+    public int getCount() {
+        return count;
+    }
+    private boolean isOver(int cur) {
+        return cur == max;
+    }
 }
