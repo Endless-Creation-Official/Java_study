57864e4415db53e37ffaba07ceeef2ad278b5776
KT <kimgt2828@gmail.com>
2024-05-17 15:59:04 +0900

    final 변수 - 중앙 제어

---
+++ 김경태/Java_basic/src/final1/Constant.java
@@ -0,0 +1,9 @@
+package final1;
+
+public class Constant {
+    public static final double PI = 3.141592;
+    public static final int HOURS_IN_DAY = 24;
+    public static final int MINUTES_IN_HOURS = 60;
+    public static final int SECONDS_IN_MINUTE = 60;
+    public static final int MAX_USERS = 1000;
+}
