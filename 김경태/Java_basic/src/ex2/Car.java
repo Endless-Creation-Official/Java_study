3cde0c093943f9e7628a73aa80ca3fad88f20ed1
KT <kimgt2828@gmail.com>
2024-05-18 22:41:51 +0900

    상속 - 도입

---
+++ 김경태/Java_basic/src/ex2/Car.java
@@ -0,0 +1,7 @@
+package ex2;
+
+public class Car {
+    public void move() {
+        System.out.println("차를 이동합니다.");
+    }
+}
---
+++ 김경태/Java_basic/src/ex2/GasCar.java
@@ -0,0 +1,7 @@
+package ex2;
+
+public class GasCar extends Car{
+    public void fillUp() {
+        System.out.println("기름을 충전합니다.");
+    }
+}
