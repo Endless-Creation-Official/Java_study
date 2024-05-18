43ed9ae81bcc54cf8834e65182f292ed3e52bd5c
KT <kimgt2828@gmail.com>
2024-05-19 01:03:51 +0900

    상속 - super

---
+++ 김경태/Java_basic/src/extends1/super1/Parent.java
@@ -0,0 +1,11 @@
+package extends1.super1;
+
+import com.sun.security.jgss.GSSUtil;
+
+public class Parent {
+    public String value = "parent";
+    public void hello() {
+        System.out.println("Parent.hello");
+    }
+
+}
