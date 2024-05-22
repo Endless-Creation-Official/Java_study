c0061cb9fe93221f927344d7493ba6f1b505d106
KT <kimgt2828@gmail.com>
2024-05-22 17:46:26 +0900

    객체지향 설계 원칙

---
+++ 김경태/Java_basic/src/poly/car0/Model3Car.java
@@ -0,0 +1,13 @@
+package poly.car0;
+
+public class Model3Car {
+    public void startEngine() {
+        System.out.println("Model3.startEngine");
+    }
+    public void offEngine() {
+        System.out.println("Model3.offEngine");
+    }
+    public void pressAccelerator() {
+        System.out.println("Model3.pressAccelerator");
+    }
+}
