3e7278d8d322a6ba7ab6355342ced2d9b91a9083
choikang <choikang7157@naver.com>
2024-05-28 18:31:20 +0900

    다형성 도입

---
+++ 최강/src/poly/basic/PolyMain.java
@@ -0,0 +1,23 @@
+package poly.basic;
+
+public class PolyMain {
+    public static void main(String[] args) {
+        // 부모 변수가 부모 인스턴스 참조
+        System.out.println("parent -> parent");
+        Parent parent = new Parent();
+        parent.parentMethod();
+
+        // 자식 변수가 자식 인스턴스 참조
+        System.out.println("child -> child");
+        Child child = new Child();
+        child.childMethod();
+
+        // 부모 변수가 자식 인스턴스 참조(다형적 참조)
+        System.out.println("parent -> child");
+        Parent poly = new Child();
+        poly.parentMethod();
+
+        //Child child1 = new Parent(); 자식은 부모를 담을 수 없다
+        //poly.childMethod(); 자식의 기능은 호출 불가능
+    }
+}
