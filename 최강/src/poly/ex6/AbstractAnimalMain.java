b340e80c8cb1d211f4c44dc89f1c24af69783724
choikang <choikang7157@naver.com>
2024-06-01 12:52:01 +0900

    정리

--- 최강/src/poly/ex6/AbstractAnimalMain.java
+++ 최강/src/poly/ex6/AbstractAnimalMain.java
@@ -14,12 +14,12 @@ public class AbstractAnimalMain {
         flyAnimal(chicken);
     }
 
-    public static void soundAnimal(AbstractAnimal animal) {
+    public static void soundAnimal(AbstractAnimal animal) { // 부모는 자식을 품을수 있어 animal=bird 메모리 구조 생각
         System.out.println("동물 소리 테스트 시작");
         animal.sound();
         System.out.println("동물 소리 테스트 종료");
     }
-    public static void flyAnimal(Fly fly) {
+    public static void flyAnimal(Fly fly) { // fly=bird 
         System.out.println("날기 테스트 시작");
         fly.fly();
         System.out.println("날기 테스트 종료");
