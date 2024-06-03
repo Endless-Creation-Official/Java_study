edbaf457e47d8887653a53478640a6f7b0546536
choikang <choikang7157@naver.com>
2024-06-03 18:30:42 +0900

    배열의 특징

---
+++ 최강/java-mid2/src/collection/array/ArrayMain2.java
@@ -0,0 +1,48 @@
+package collection.array;
+
+import java.util.Arrays;
+
+public class ArrayMain2 {
+    public static void main(String[] args) {
+        int[] arr = new int[5];
+        arr[0] = 1;
+        arr[1] = 2;
+        System.out.println(Arrays.toString(arr));
+
+        //배열의 첫번째 위치에 추가
+        // 기번 배열의 데이터를 한 칸씩 뒤로 밀고 배열의 첫번째 위치에 추가
+        System.out.println("배열의 첫번째 위치에 3추가 0(n)");
+        int newValue = 3;
+        addFirst(arr, newValue);
+        System.out.println(Arrays.toString(arr));
+
+        //index 위치에 추가
+        // 기본 배열의 데이터를 한 칸씩 뒤로 밀고 배열의 index 위치에 추가
+        System.out.println("배열의 index(2) 위치에 4 추가 o(n)");
+        int index = 2;
+        int value = 4;
+        addAtIndex(arr, index, value);
+        System.out.println(Arrays.toString(arr));
+
+        System.out.println("배열의 마지막 위치에 5추가");
+        addLast(arr, 5);
+    }
+
+    private static void addLast(int[] arr, int newValue) {
+        arr[arr.length - 1] = newValue;
+    }
+
+    private static void addAtIndex(int[] arr, int index, int newValue) {
+        for (int i = arr.length - 1; i > index; i--) {
+            arr[i] = arr[i - 1];
+        }
+        arr[2] = newValue;
+    }
+
+    private static void addFirst(int[] arr, int newValue) {
+        for (int i = arr.length - 1; i > 0; i--) {
+            arr[i] = arr[i - 1];
+        }
+        arr[0] = newValue;
+    }
+}
