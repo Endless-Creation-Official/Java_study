652bded1bedaf0546d3a6d1948454904acd62273
JiinHong <com5942@seoultech.ac.kr>
2024-06-13 18:55:06 +0900

    등록 & 조회

--- 박진홍/hello-spring/src/main/java/hello/hello_spring/service/MemberService.java
+++ 박진홍/hello-spring/src/main/java/hello/hello_spring/service/MemberService.java
@@ -3,10 +3,13 @@ package hello.hello_spring.service;
 import hello.hello_spring.domain.Member;
 import hello.hello_spring.repository.MemberRepository;
 import hello.hello_spring.repository.MemoryMemberRepository;
+import org.springframework.stereotype.Controller;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
 import java.util.Optional;
 
+@Service
 public class MemberService {
 
     private final MemberRepository memberRepository;
