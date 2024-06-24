f14fd60911528e1c2cd727e9eaa7b54373c8af23
JiinHong <com5942@seoultech.ac.kr>
2024-06-24 17:59:40 +0900

    회원 주문

---
+++ 박진홍/core/src/main/java/hello/core/member/MemberServiceImpl.java
@@ -0,0 +1,18 @@
+package hello.core.member;
+
+
+// 서비스 구현체
+public class MemberServiceImpl implements MemberService {
+
+    private final MemberRepository memberRepository = new MemoryMemberRepository();
+
+    @Override
+    public void join(Member member) {
+        memberRepository.save(member);
+    }
+
+    @Override
+    public Member findMember(Long memberId) {
+        return memberRepository.findById(memberId);
+    }
+}
